6a1c022979018c77b0d457f59d3ae96f88d10c15
Monica Tang <monicaa.tang@gmail.com>
2018-05-29 22:21:01 -0700

    added source labels and more website structure

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
 const url = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&' +
-          'apiKey=d418a65c0c38453da8d0ee0eae5467e0';
+          'apiKey=d418a65c0c38453da8d0ee0eae5467e0'
 
-
+var counter = 0;
 
 $(document).ready(function(){
 
@@ -13,24 +13,24 @@ $(document).ready(function(){
 	.then((resp) => resp.json())
 	.then(function(data){
 		for (var i = 0; i < data.articles.length; i++) {
-			var newsfeed = document.getElementsByClassName("newsfeed");
+			var newsfeed = document.getElementsByClassName("row");
 			var curr = data.articles[i];
-			var row = 0;
-			var counter = 0;
-			if(counter % 3 === 0 && counter > 0){
-				row += 1;
-				document.getElementById("rows").insertAdjacentHTML('beforeend', "<div class = 'row newsfeed'></div>");
-				newsfeed = document.getElementsByClassName("newsfeed");
+			if(counter>=9){
+				break;
 			}
-
 			if(curr.urlToImage){
-				newsfeed[row].insertAdjacentHTML('beforeend', 
+				newsfeed[0].insertAdjacentHTML('beforeend', 
 				`<div class= "col-md-4">
 						<a href = "${curr.url}">
-						<div class = "card mb-4 box-shadow">
-							<img class = "card-img-top" src="${curr.urlToImage}">
+						<div style="border-style: none" class = "card mb-4 box-shadow">
+							<div class = "img_container">
+								<img class = "card-img-top" src="${curr.urlToImage}">
+								<div class="text-block"> 
+							    	<h4>${curr.source.name}</h4>
+							  	</div>
+						  	</div>
 							<div class = "card-body">
-								<p class = "card-text"> <h4>${curr.title}<h4></p>
+								<p class = "card-text"><h4>${curr.title}</h4></p>
 							</div>
 						</div>
 					</div>
@@ -38,6 +38,7 @@ $(document).ready(function(){
 			} else {
 				counter -= 1;
 			}
+
 			counter++;
 			
 		}
